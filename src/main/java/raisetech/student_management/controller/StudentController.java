1f73751357c651e1a33c4a84db9afa7867113250
yassan-g <exilev1courage@icloud.com>
2025-10-13 01:00:55 +0900

    Thymeleafを使ったReadの画面描画処理を実装

--- src/main/java/raisetech/student_management/controller/StudentController.java
+++ src/main/java/raisetech/student_management/controller/StudentController.java
@@ -1,15 +1,15 @@
 package raisetech.student_management.controller;
 
 import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import raisetech.student_management.controller.converter.StudentConverter;
 import raisetech.student_management.data.Student;
 import raisetech.student_management.data.StudentCourse;
-import raisetech.student_management.domain.StudentDetail;
 import raisetech.student_management.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
   private final StudentService studentService;
@@ -21,11 +21,13 @@ public class StudentController {
   }
 
   @GetMapping("/students")
-  public List<StudentDetail> getStudents() {
+  public String getStudents(Model model) {
     List<Student> students = studentService.searchStudents();
     List<StudentCourse> studentCourses = studentService.searchStudentCourses();
 
-    return studentConverter.convertStudentDetails(students, studentCourses);
+    model.addAttribute("students", studentConverter.convertStudentDetails(students, studentCourses));
+
+    return "students.html";
   }
 
   @GetMapping("/student-courses")
