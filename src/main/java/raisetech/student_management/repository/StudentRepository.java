f042d1447f786d284a1835bfdc9e7b4352ac755d
yassan-g <exilev1courage@icloud.com>
2025-10-26 21:31:34 +0900

    ブラウザから新規受講生情報とコース情報を登録する処理を実装

--- src/main/java/raisetech/student_management/repository/StudentRepository.java
+++ src/main/java/raisetech/student_management/repository/StudentRepository.java
@@ -1,7 +1,9 @@
 package raisetech.student_management.repository;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import raisetech.student_management.data.Student;
 import raisetech.student_management.data.StudentCourse;
@@ -24,5 +26,15 @@ public interface StudentRepository {
    */
   @Select("SELECT * FROM students_courses")
   List<StudentCourse> findAllStudentCourses();
+
+  @Insert("INSERT INTO students(name, name_kana, nickname, email, area, birth_date, gender, remark, is_deleted)"
+      + "VALUES(#{name}, #{nameKana}, #{nickname}, #{email}, #{area}, #{birthDate}, #{gender}, #{remark}, false)")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudent(Student student);
+
+  @Insert("INSERT INTO students_courses(student_id, course_name, start_date, expected_end_date)"
+      + "VALUES(#{studentId}, #{courseName}, #{startDate}, #{expectedEndDate})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudentCourses(StudentCourse studentCourse);
 
 }
--- src/main/java/raisetech/student_management/service/StudentService.java
+++ src/main/java/raisetech/student_management/service/StudentService.java
@@ -1,9 +1,12 @@
 package raisetech.student_management.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import raisetech.student_management.data.Student;
 import raisetech.student_management.data.StudentCourse;
+import raisetech.student_management.domain.StudentDetail;
 import raisetech.student_management.repository.StudentRepository;
 
 @Service
@@ -21,6 +24,33 @@ public class StudentService {
 
   public List<StudentCourse> searchStudentCourses() {
     return  studentRepository.findAllStudentCourses();
+  }
+
+  @Transactional
+  public void registerStudent(StudentDetail studentDetail) {
+    studentRepository.registerStudent(studentDetail.getStudent());
+    // コース情報登録を行う
+    for (StudentCourse studentCourse : studentDetail.getStudentCourses()) {
+      studentCourse.setStudentId(studentDetail.getStudent().getId());
+      studentCourse.setStartDate(LocalDate.now());
+      switch (studentCourse.getCourseName()) {
+        case "HTML/CSS/JavaScriptコース":
+        case "SQLコース":
+          studentCourse.setExpectedEndDate(LocalDate.now().plusMonths(2));
+          break;
+        case "AWSコース":
+          studentCourse.setExpectedEndDate(LocalDate.now().plusMonths(3));
+          break;
+        case "Pythonコース":
+          studentCourse.setExpectedEndDate(LocalDate.now().plusMonths(4));
+          break;
+        case "Javaコース":
+          studentCourse.setExpectedEndDate(LocalDate.now().plusMonths(6));
+          break;
+      }
+
+      studentRepository.registerStudentCourses(studentCourse);
+    }
   }
 
 }
