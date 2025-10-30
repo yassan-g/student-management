ee6edab2b92c13e6fc3a20c26539b52a3834076f
yassan-g <exilev1courage@icloud.com>
2025-10-30 09:50:12 +0900

    更新画面の実装、登録と一覧画面を修正

--- src/main/java/raisetech/student_management/data/Student.java
+++ src/main/java/raisetech/student_management/data/Student.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
@@ -15,6 +16,7 @@ public class Student {
   private String nickname;
   private String email;
   private String area;
+  @DateTimeFormat(pattern = "yyyy/MM/dd")
   private LocalDate birthDate;
   private String gender;
   private String remark;
--- src/main/java/raisetech/student_management/repository/StudentRepository.java
+++ src/main/java/raisetech/student_management/repository/StudentRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import raisetech.student_management.data.Student;
 import raisetech.student_management.data.StudentCourse;
 
@@ -19,22 +20,41 @@ public interface StudentRepository {
   @Select("SELECT * FROM students")
   List<Student> findAllStudents();
 
+  // 受講生情報を1件取得する
+  @Select("SELECT * FROM students WHERE id = #{id}")
+  Student findStudentById(Integer id);
+
   /**
-   * 受講生コース情報を全件取得する
+   * 受講コース情報を全件取得する
    *
    * @return StudentCourseのリスト
    */
   @Select("SELECT * FROM students_courses")
-  List<StudentCourse> findAllStudentCourses();
+  List<StudentCourse> findAllStudentCourse();
 
+  // 受講コース情報を複数件取得する
+  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+  List<StudentCourse> findCoursesByStudentId(Integer studentId);
+
+  // 受講生情報を登録する
   @Insert("INSERT INTO students(name, name_kana, nickname, email, area, birth_date, gender, remark, is_deleted)"
       + "VALUES(#{name}, #{nameKana}, #{nickname}, #{email}, #{area}, #{birthDate}, #{gender}, #{remark}, false)")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudent(Student student);
 
+  // 受講コース情報を登録する
   @Insert("INSERT INTO students_courses(student_id, course_name, start_date, expected_end_date)"
       + "VALUES(#{studentId}, #{courseName}, #{startDate}, #{expectedEndDate})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
-  void registerStudentCourses(StudentCourse studentCourse);
+  void registerStudentCourse(StudentCourse studentCourse);
+
+  // 受講生情報を更新する
+  @Update("UPDATE students SET name = #{name}, name_kana = #{nameKana}, nickname = #{nickname}, email = #{email},"
+      + "area = #{area}, birth_date = #{birthDate}, gender = #{gender}, remark = #{remark}, is_deleted = #{isDeleted} WHERE id = #{id}")
+  void updateStudent(Student student);
+
+  // 受講コース情報を更新する
+  @Update("UPDATE students_courses SET course_name = #{courseName}, expected_end_date = #{expectedEndDate} WHERE id = #{id} AND student_id = #{studentId}")
+  void updateStudentCourse(StudentCourse studentCourse);
 
 }
