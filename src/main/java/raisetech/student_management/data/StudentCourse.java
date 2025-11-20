7d02e95a79b06100485887954fd0e86b02622db2
yassan-g <exilev1courage@icloud.com>
2025-11-20 15:47:18 +0900

    StudentCourseクラスのstudentIdとstartDateの入力チェックのアノテーションを削除

--- src/main/java/raisetech/student_management/data/StudentCourse.java
+++ src/main/java/raisetech/student_management/data/StudentCourse.java
@@ -1,7 +1,6 @@
 package raisetech.student_management.data;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import lombok.Getter;
@@ -15,14 +14,12 @@ public class StudentCourse {
 
   private Integer id;
 
-  @NotNull(message = "受講生IDは必須です")
   private Integer studentId;
 
   @NotBlank(message = "コース名は必須です")
   @Size(max = 50, message = "コース名は50文字以内で入力してください")
   private String courseName;
 
-  @NotNull(message = "開始日は必須です")
   private LocalDate startDate;
 
   private LocalDate expectedEndDate;
